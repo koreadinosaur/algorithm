45e5c77742829e0777ecbe0df23e2d61a73f448f
HyeongJin Kim <khj7741@gmail.com>
2022-09-19 23:11:07 +0900

    백준 재귀 25501

--- javascriptAlgorithm/recursion/isPalindrome.js
+++ javascriptAlgorithm/recursion/isPalindrome.js
@@ -3,8 +3,9 @@ function isPalindrome(string) {
   console.log(string);
   if (string.length <= 1) return true;
   if (string[0] !== string[string.length - 1]) return false;
-  const newString = string.substring(1);
-  return isPalindrome(newString.slice(0, newString.length - 1));
+  // const newString = string.substring(1);
+  // return isPalindrome(newString.slice(0, newString.length - 1));
+  return isPalindrome(string.slice(1, -1));
 }
 
 const test = isPalindrome("amanaplanacanalpanama");
