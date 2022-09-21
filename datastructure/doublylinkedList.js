9333f1e2e788284817524157dc90d2bc00b09075
HyeongJin Kim <khj7741@gmail.com>
2022-09-21 23:16:19 +0900

    이중 연결리스트 class 생성, push메서드 생성

---
+++ datastructure/doublylinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const DblLinkedList = new DoublyLinkedList();
+DblLinkedList.push(1);
+DblLinkedList.push(2);
+DblLinkedList.push(3);
+console.log(DblLinkedList);
