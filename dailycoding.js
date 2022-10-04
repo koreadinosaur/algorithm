4e02ec342800e0d04ef6b5633b38a669c59358d2
HyeongJin Kim <khj7741@gmail.com>
2022-10-05 08:57:37 +0900

    코드스테이츠 데일리코딩 11번

--- dailycoding.js
+++ dailycoding.js
@@ -1,12 +1,18 @@
-/* 코플릿 10번 */
-function insertDash(str) {
-  let array = str.split("");
-  let result = array.map((item, index) => {
-    if (Number(item) % 2 !== 0 && Number(array[index - 1] % 2 !== 0)) {
-      return `-${item}`;
-    }
-    return item;
-  });
-  return result.join("");
+/* 코플릿 11번 */
+function removeExtremes(arr) {
+  // TODO: 여기에 코드를 작성합니다.
+  let minLengthIndex = 0;
+  let maxLengthIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    minLengthIndex =
+      arr[minLengthIndex].length >= arr[i].length ? i : minLengthIndex;
+    maxLengthIndex =
+      arr[maxLengthIndex].length <= arr[i].length ? i : maxLengthIndex;
+  }
+  return arr.filter(
+    (item, index) => index !== minLengthIndex && index !== maxLengthIndex
+  );
 }
-console.log(insertDash("454793"));
+
+const test = removeExtremes(["a", "b", "c", "def"]);
+console.log(test);
