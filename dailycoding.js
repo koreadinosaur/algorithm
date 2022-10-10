6400d2dc58da5bff9b6fabb5ed7fd11951647fe7
HyeongJin Kim <khj7741@gmail.com>
2022-10-10 22:31:43 +0900

    코플릿 14번

--- dailycoding.js
+++ dailycoding.js
@@ -18,3 +18,22 @@ output = findBugInApples([
   ["A", "A", "A", "A", "A"],
 ]);
 console.log(output); //[1, 1]
+
+/* 14번 superIncreasing 
+function superIncreasing(arr) {
+  // TODO: 여기에 코드를 작성합니다.
+  let isLargerThanSum = true;
+  for (let i = 1; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i - 1; j >= 0; j--) {
+      sum += arr[j];
+    }
+    if (sum >= arr[i]) {
+      isLargerThanSum = false;
+      break;
+    }
+  }
+  return isLargerThanSum;
+}
+output = superIncreasing([1, 3, 5, 9]);
+console.log(output); */
